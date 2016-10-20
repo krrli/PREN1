3ac9baff9fcd6e6634f298097ff479b277f26389
chessrookie <glaus_marco@hotmail.com>
2016-10-20 10:22:20 +0200

    add some code

---
+++ src/ocr.py
@@ -0,0 +1,19 @@
+#erster Versuch
+
+import cv2
+from PIL import Image
+import pytesseract
+import numpy as np
+
+#print pytesseract.image_to_string(Image.open('roemisch2.PNG'))
+
+#path = './bilder/roemisch2.PNG'
+path = './bilder/randomzahlen.jpg'
+
+img = Image.open(path)
+print (pytesseract.image_to_string(img))
+
+image = cv2.imread(path)
+cv2.imshow('hallo', image)
+cv2.waitKey(0)
+
